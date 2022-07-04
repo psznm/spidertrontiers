399483ce5d13222f324bd231e2f312f2047ae6be
psznm <psznm@seznam.cz>
2022-07-04 16:05:10 +0200

    Processed fuel for aai-industry

--- lib/fuel.lua
+++ lib/fuel.lua
@@ -7,11 +7,34 @@ local fuelTiers = {
     { 'chemical' },
     { 'chemical' },
 }
+
+-- should do this by inserting and removing, instead of hardcoding each possible combination of mods,
+-- but right now i dont know how and it is good enough for now.
 if mods["Krastorio2"] then
     fuelTiers = {
         { 'chemical' },
         { 'chemical', "vehicle-fuel" },
         { "chemical", "vehicle-fuel", 'nuclear-fuel', 'nuclear' },
+        { 'nuclear-fuel', 'fusion-fuel', 'nuclear' },
+        { 'fusion-fuel' },
+        { 'antimatter-fuel' },
+    }
+end
+if mods["aai-industry"] then
+    fuelTiers = {
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "processed-chemical" },
+    }
+end
+if mods["Krastorio2"] and mods["aai-industry"] then
+    fuelTiers = {
+        { 'chemical', "processed-chemical" },
+        { 'chemical', "vehicle-fuel", "processed-chemical" },
+        { "chemical", "vehicle-fuel", 'nuclear-fuel', 'nuclear', "processed-chemical" },
         { 'nuclear-fuel', 'fusion-fuel', 'nuclear' },
         { 'fusion-fuel' },
         { 'antimatter-fuel' },
